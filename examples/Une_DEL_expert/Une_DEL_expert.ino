fbdab7f57d2ab4dca37bb186ad83bf0f0af2bd64
TechnoPhysCAL <claude.bouchard@claurendeau.qc.ca>
2020-08-26 19:46:46 -0400

    Ajustement pour démo brightness

--- examples/Une_DEL_expert/Une_DEL_expert.ino
+++ examples/Une_DEL_expert/Une_DEL_expert.ino
@@ -1,12 +1,13 @@
 /*
   Librairie TPG DEL - Une_DEL_expert.ino
 
-  Fait la démonstration d'une DEL en utilisant le mode clignotement sur un cycle de 9 secondes:
+  Fait la démonstration d'une DEL en utilisant le mode clignotement sur un cycle de 12 secondes:
    - 3 secondes allumée;
    - 3 secondes allumée clignotante;
+   - 3 secondes allumée où l'intensité lumineuse varie de 0 à 100%;
    - 3 secondes éteintes.
   
-  Note:  La fonction .setBlinking(bool) active ou désactive le mode 'clignotement' : ELLE N'A PAS D'EFFET SUR L'ALLUMAGE OU NON DE LA DEL.
+  Note:  La fonction .setBlinking(bool) active ou désactive le mode 'clignotement' : Elle n'a pas d'effet sur l'allumage ou non de la DEL.
 
 */
 
@@ -18,7 +19,6 @@ Del maDEL(pin); // Initialisation de la DEL sur le port choisi
 
 void setup()
 {
-
   maDEL.setBlinkingInterval(100); //Ajuste le délai de clignotement à 150 millisecondes (Par défault, 250 millisecondes).
 }
 
@@ -26,19 +26,26 @@ void loop()
 {
   maDEL.refresh(); //Permet d'actualiser l'état de la DEL. CETTE MÉTHODE EST OBLIGATOIRE EN DÉBUT DE LOOP(), IDÉALEMENT.
 
-  unsigned long current_time = millis() % 9000; // Un temps toujours situé entre 0 et 9 secondes.
+  unsigned long current_time = millis() % 12000; // Un temps toujours situé entre 0 et 12 secondes.
 
   if (current_time < 3000) // Est VRAIE si le temps est inférieur à 3 secondes;
   {
-    maDEL.set(true);          // Allume la DEL.
+    maDEL.set(true); // Allume la DEL.
   }
-  else if (current_time < 6000) // Est VRAIE si le temps est supérieur à 3 et inférieur à 6 secondes.
+  else if (current_time < 6000)
   {
     maDEL.setBlinking(true); //Active le mode clignotement.
   }
-  else // Sinon, après 6 secondes.
+  else if (current_time < 9000) // Est VRAIE si le temps est supérieur à 3 et inférieur à 9 secondes.
   {
-    maDEL.set(false); // Éteint la DEL.
-    maDEL.setBlinking(false); //Désactive le mode clignotement.
+    maDEL.setBlinking(false);
+    float percentage = (current_time-6000)/3000.0 *100.0 ; //Calcule une valeur variant de 0 à 100, sur 3 secondes.
+    maDEL.setBrightness(percentage); //Ajuste la luminosité de la Del selon le pourcentage calculé, entre 0 et 100.
+
+  }
+  else
+  {                           // Sinon, après 6 secondes.
+    maDEL.set(false);         // Éteint la DEL.
+    
   }
 }
