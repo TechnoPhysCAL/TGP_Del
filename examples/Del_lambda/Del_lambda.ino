3771757747765f6989f9abcd5c962e5be4fe9ce1
Claude Bouchard <dualB@users.noreply.github.com>
2025-01-21 09:07:54 -0500

    correction exemple

--- examples/Del_lambda/Del_lambda.ino
+++ examples/Del_lambda/Del_lambda.ino
@@ -3,7 +3,7 @@
 
   ###DESCRIPTION
   Cet exemple permet d'utiliser une lambda pour définir la fonction de mise à jour de la DEL.
-  La fonction de mise à jour de la DEL est définie dans la fonction setUpdateFunction.
+  La fonction de mise à jour de la DEL est définie dans la fonction setSignalUpdater.
 
   ###NOTE
   Dans cet exemple, la DEL est simulée par des messages au port série.
@@ -11,17 +11,19 @@
 
 #include <Del.h> // Pour utiliser la librairie Del
 
-#define PIN 2 // Le numéro du port de la DEL.
-
-DelPin maDel(PIN); // Initialisation de la DEL sur le port choisi
+Del maDel; // Initialisation de la DEL générique
 
 void setup()
 {
+  Serial.begin(115200); // Initialiser la communication série
+
   maDel.setBlinking(true); // Activer le mode clignotement pour la DEL
-  maDel.setBrightness(100);
-  maDel.setUpdateFunction([](float percentage) {
+  maDel.setBrightness(100); //Mettre l'intensité de la DEL à 100%
+  maDel.setSignalUpdater([](float percentage) {
     Serial.println("Ma del est à un pourcentage de " + String(percentage));
-  }); // Définit la fonction de mise à jour de la DEL
+  }); // Définit la fonction de mise à jour de la DEL (dans l'exemple, un message est envoyé au port série pour simuler la Del)
+
+   maDel.on(); // Allume la DEL
 }
 
 void loop()
